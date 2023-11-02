10cf06f42b44f3a00e737e4e85f83d18f20e9343
Joanerson Costa <joanersonc31@gmail.com>
2023-11-02 16:52:47 -0300

    implementa pet domain

---
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -0,0 +1,51 @@
+package br.com.petz.clientepet.pet.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.apache.el.parser.AstInteger;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Pet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idPet;
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idCliente;
+	@NotBlank
+	private String nomePet;
+	@Enumerated(EnumType.STRING)
+	private Porte porte;
+	@Enumerated(EnumType.STRING)
+	private TipoPet tipo;
+	private String microShip;
+	private String raca;
+	@Enumerated(EnumType.STRING)
+	private SexoPet sexo;
+	private String pelagemCor;
+	@NotNull
+	private LocalDate dataNascimento;
+	private String rga;
+	private int peso;
+
+	private LocalDateTime momentoDoDacastro;
+	private LocalDateTime dataHoraDaultimaAlteracao;	
+}
